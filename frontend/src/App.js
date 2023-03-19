beed7dd7ec70c59718012d0cb11db55e43a277b3
allforest01 <allforest01@gmail.com>
2023-03-19 15:42:10 +0700

    Fix bug

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import Starter from "./components/Starter/Starter";
 import Section from "./components/Section/Section";
+import Intro from "./components/Intro/Intro";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Starter />} />
           <Route path="/section" element={<Section />} />
+          <Route path="/intro" element={<Intro />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ frontend/src/components/Intro/Intro.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import intro from './intro.png';
+
+class Intro extends React.Component {
+    render() {
+        return <>
+            <img src={intro} alt="Intro" />
+        </>;
+    }
+}
+
+export default Intro;
